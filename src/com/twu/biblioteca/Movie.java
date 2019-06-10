18b7a5d6fc235f455a7893dd301677bb90a03066
Michelle Peralbo <michelle.peralvo@thoughtworks.com>
2019-06-10 17:35:53 -0500

    Biblioteca Database

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca;
+
+public class Movie {
+    private String name;
+    private int year;
+    private String director;
+    private int movieRating;
+
+}
---
+++ src/com/twu/biblioteca/User.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+public class User {
+    private int idUsuario;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String libraryNumber;
+
+
+}
